a5fcca00d12d91d0bd29d435d552ebd70b6c0a82
mehdidotcafe <contact@mehdi.cafe>
2023-03-23 23:40:16 +0100

    3/23/2023

---
+++ hackerrank/implementation/weighted-uniform-string.hs
@@ -0,0 +1,20 @@
+import Data.List (group, find, unlines)
+import Control.Arrow ((&&&))
+import Debug.Trace (trace)
+import System.Environment ( getEnv )
+import System.IO
+    ( hClose, hFlush, hPutStrLn, openFile, IOMode(WriteMode) )
+
+aAsciiValue = fromEnum 'a'
+
+writeInFile str = getEnv "OUTPUT_PATH" >>= \filepath -> writeFile filepath str
+
+cmp letters v = case find (\ (letterWeight, letterCount) -> v `rem` letterWeight == 0 && v <= letterWeight * letterCount) letters of
+  Nothing -> "No"
+  _ -> "Yes"
+
+main = getContents >>= writeInFile .
+  unlines .
+  ( \ (letters, values) -> map (cmp letters) values) .
+  (( map (\ x -> (fromEnum (head x) - aAsciiValue + 1, length x)). group . head) &&& (map (read :: String -> Int) . (tail . tail))) .
+  words
